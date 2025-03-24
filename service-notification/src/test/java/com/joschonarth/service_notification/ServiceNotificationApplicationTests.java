9c5433dec86429cc67a1e6de5b680d69511bca3c
joschonarth <joschonarth@gmail.com>
2025-03-23 21:37:46 -0300

    feat: setup notification service

---
+++ service-notification/src/test/java/com/joschonarth/service_notification/ServiceNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joschonarth.service_notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
